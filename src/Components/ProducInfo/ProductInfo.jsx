a60fb5c533addece581dee57cb3a6d81b9eb9c00
sky2d <143280010+sky2d@users.noreply.github.com>
2024-07-14 12:53:21 +0530

    ADD navigation to community page

--- src/Components/ProducInfo/ProductInfo.jsx
+++ src/Components/ProducInfo/ProductInfo.jsx
@@ -1,28 +1,34 @@
 import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Products } from "../../Utilities/ProductData";
+import { useNavigate } from "react-router-dom";
 import img1 from "../../assets/img1.jpeg";
 import "./Info.css"
 import { MyContext } from "../../Utilities/Context/ContextProvider";
 import { FaRegHeart } from "react-icons/fa";
 
 const ProductInfo = () => {
-  console.log(Products);
-  const { id } = useParams(); 
-  const product = Products.find((product) => product.product_title === id);
-  console.log(product);
-  const { outfit, setOutfit } = useContext(MyContext);
-  const handleImageChange = (e) => {
-    const newOutfit = e.target.src;
-    setOutfit((prevOutfit) => [...prevOutfit, newOutfit]);
-  };
+   const { id } = useParams();
+   const navigate = useNavigate();
+   const product = Products.find((product) => product.product_title === id);
+   const { outfit, setOutfit } = useContext(MyContext);
+
+   const handleNavigate = () => {
+     setOutfit((prevImageSrc) => [...prevImageSrc, product.imgUrl]);
+     navigate("/outfitmaker");
+   };
+  
+  console.log(outfit)
   return (
     <>
       {product && (
         <div className="container">
           <div className="product-page">
             <div className="product-images">
-              <img src={product.imgUrl} alt="Product Image" />
+              <img
+                src={product.imgUrl}
+                alt="Product Image"
+              />
             </div>
             <div className="product-details">
               <h1>{product.brand_name}</h1>
@@ -42,11 +48,10 @@ const ProductInfo = () => {
                   ADD TO BAG
                 </button>
                 <button className="text-base p-2 bg-slate-300 text-white border-1 bo">
-                  
-                   WISHLIST
-                  
+                  WISHLIST
                 </button>
-                <button className="text-base p-2 bg-[#4caf50] text-white">
+
+                <button onClick={handleNavigate} className="text-base p-2 bg-[#4caf50] text-white">
                   CREATE OUTFIT
                 </button>
               </div>
